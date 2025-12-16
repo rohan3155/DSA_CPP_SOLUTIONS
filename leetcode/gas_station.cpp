221a07cb92975f16c258d98126f8f45ff16854e8
rohan3155 <bhatiarohan050@gmail.com>
2025-12-16 08:40:45 +0530

    Day-16 Question-2

---
+++ leetcode/gas_station.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int totalGas = 0, totalCost = 0;
+
+        for (int val : gas) {
+            totalGas += val;
+        }
+        for (int val : cost) {
+            totalCost += val;
+        }
+        if (totalGas < totalCost) {
+            return -1;
+        }
+
+        int start = 0, currGas = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            currGas += (gas[i] - cost[i]);
+
+            if (currGas < 0) {
+                start = i + 1;
+                currGas = 0;
+            }
+        }
+        return start;
+    }
+};
+
+
+// or
+
+
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int totalGas = 0, totalCost = 0;
+
+        int start = 0, currGas = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+
+            currGas += (gas[i] - cost[i]);
+
+            if (currGas < 0) {
+                start = i + 1;
+                currGas = 0;
+            }
+        }
+
+        return totalGas < totalCost ? -1 : start;
+    }
+};
