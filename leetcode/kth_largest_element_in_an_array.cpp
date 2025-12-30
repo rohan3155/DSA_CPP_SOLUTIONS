b13175797cc5855991107c9425f6849669b52ccc
rohan3155 <bhatiarohan050@gmail.com>
2025-12-30 07:49:50 +0530

    Day-27 Question-1

---
+++ leetcode/kth_largest_element_in_an_array.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+        int findKthLargest(vector<int> &nums, int k)
+        {
+                priority_queue<int, vector<int>, greater<int>> min_heap;
+
+                for (int el : nums)
+                {
+                        min_heap.push(el);
+
+                        if (min_heap.size() > k)
+                        {
+                                min_heap.pop();
+                        }
+                }
+
+                return min_heap.top();
+        }
+};
